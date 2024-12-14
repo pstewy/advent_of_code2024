b516fb5db504557e517a1ddcdcbbc300dd2c93fe
Parker Stewart <parkercstewart97@gmail.com>
2024-12-14 09:33:00 -0700

    day 11 part 2

---
+++ day11/go.mod
@@ -0,0 +1,3 @@
+module github.com/pstewy/advent2024/day11
+
+go 1.23.4
--- day11/main.go
+++ day11/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 func main() {
@@ -17,8 +16,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(part2(stones, 25))
-
+	baseStones := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	tracker := map[int]map[int]int{}
+	for _, stone := range baseStones {
+		fmt.Println("building cache for", stone)
+		blinkTracker := map[int]int{}
+		stoneBuilder := []int{stone}
+		for i := range 41 {
+			idx := i + 1
+			stoneBuilder = splitStones(stoneBuilder)
+			blinkTracker[idx] = len(stoneBuilder)
+		}
+		tracker[stone] = blinkTracker
+	}
+	fmt.Println("starting problem")
+	fmt.Println(splitStonesMemo(stones, 75, tracker))
 }
 
 func parse(f []byte) ([]int, error) {
@@ -34,46 +46,81 @@ func parse(f []byte) ([]int, error) {
 }
 
 // splitStones is too slow for large quantitys. split the work up
-func part2(stones []int, blinks int) int {
-	wg := sync.WaitGroup{}
-	count := 0
-	lock := sync.Mutex{}
+// func part2(stones []int, blinks int) int {
+// 	wg := sync.WaitGroup{}
+// 	count := 0
+// 	lock := sync.Mutex{}
+// 	for _, stone := range stones {
+// 		wg.Add(1)
+// 		go func() {
+// 			temp := splitStones([]int{stone}, blinks)
+// 			lock.Lock()
+// 			count += temp
+// 			lock.Unlock()
+// 			wg.Done()
+// 		}()
+// 	}
+// 	wg.Wait()
+// 	return count
+// }
+
+func splitStones(stones []int) []int {
+	var tempStones []int
 	for _, stone := range stones {
-		wg.Add(1)
-		go func() {
-			temp := splitStones([]int{stone}, blinks)
-			lock.Lock()
-			count += temp
-			lock.Unlock()
-			wg.Done()
-		}()
+		parts := splitStone(stone)
+		switch {
+		case stone == 0:
+			tempStones = append(tempStones, 1)
+		case len(parts)%2 == 0:
+			length := len(parts)
+			firstHalf := parts[:length/2]
+			secondHalf := parts[length/2:]
+			tempStones = append(tempStones, intoStone(firstHalf), intoStone(secondHalf))
+		default:
+			tempStones = append(tempStones, stone*2024)
+		}
 	}
-	wg.Wait()
-	return count
+
+	return tempStones
 }
 
-func splitStones(stones []int, blinks int) int {
-	for i := range blinks {
-		fmt.Println("splitting...", i)
-		var tempStones []int
-		for _, stone := range stones {
-			parts := splitStone(stone)
-			switch {
-			case stone == 0:
-				tempStones = append(tempStones, 1)
-			case len(parts)%2 == 0:
-				length := len(parts)
-				firstHalf := parts[:length/2]
-				secondHalf := parts[length/2:]
-				tempStones = append(tempStones, intoStone(firstHalf), intoStone(secondHalf))
-			default:
-				tempStones = append(tempStones, stone*2024)
-			}
-		}
-		stones = tempStones
+// Store map of stone value -> blinks -> eventual length
+func splitStonesMemo(stones []int, blinksRemaning int, computedValues map[int]map[int]int) int {
+	if blinksRemaning == 0 || len(stones) == 0 {
+		return len(stones)
 	}
 
-	return len(stones)
+	var tempStones []int
+	var total int
+	for _, stone := range stones {
+		// Have we seen this stone before?
+		blinksForStone, ok := computedValues[stone]
+		if ok {
+			// We've seen this stone before, have we seen the
+			// blink count?
+			countForBlinks, ok := blinksForStone[blinksRemaning]
+			if ok {
+				total += countForBlinks
+				fmt.Println("memoized!", stone, countForBlinks)
+				continue
+			}
+		}
+		// fmt.Println("splitting... remaining", blinksRemaning, stone)
+		parts := splitStone(stone)
+		switch {
+		case stone == 0:
+			tempStones = append(tempStones, 1)
+		case len(parts)%2 == 0:
+			length := len(parts)
+			firstHalf := parts[:length/2]
+			secondHalf := parts[length/2:]
+			tempStones = append(tempStones, intoStone(firstHalf), intoStone(secondHalf))
+		default:
+			tempStones = append(tempStones, stone*2024)
+		}
+
+	}
+	return total + splitStonesMemo(tempStones, blinksRemaning-1, computedValues)
 }
 
 func splitStone(stone int) []int {
